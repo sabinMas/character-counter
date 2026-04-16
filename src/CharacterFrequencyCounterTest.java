72b755d175af479f271b89deec85b63332a75c0d
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-04-16 11:13:10 -0700

    code along in class

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,23 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+
+@Test
+void testCharacterShowsUpMultipleTimesGivesCorrectFrequency(){
+    //arrange 
+    CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+    //act
+    int actual = counter.getFrequency('l');
+    //assert
+    assertEquals(2, actual);
 }
+@Test
+void testGetRelativeFrequencyReturns0ForMissingCharacter(){
+    //arrange
+    CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+    //act
+    double actual = counter.getRelativePercentage('x');
+    //assert
+    assertEquals(0.0, actual, 0.0001); 
+}
+}    
